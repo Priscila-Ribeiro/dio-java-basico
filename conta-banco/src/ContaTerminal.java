90b6a93bb295b8f469e5116c7853ca6d73db4300
Priscila Ribeiro <ddarcoliveira@gmail.com>
2024-05-31 21:38:48 -0300

    Consulta saldo da Conta Bancaria

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,6 +1,33 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
 
+   
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        
+        System.out.print("Por favor, digite o número da Agência: ");
+        String agencia = scanner.next();
+
+
+        System.out.print("Por favor, digite o número da sua conta: ");
+        int conta = scanner.nextInt();
+
+
+        System.out.print("Por favor, digite o seu primeiro nome: ");
+        String cliente = scanner.next();
+        
+        // Consulta o saldo da conta
+
+        final double saldo = 5000;
+      
+
+System.out.println("Olá " + cliente + ", obrigado por criar uma conta em nosso banco.");
+System.out.println("sua agência é " + agencia + ".");
+System.out.println("Sua conta é " + conta + "."); 
+System.out.println("Seu saldo de U$ " + saldo + " e já está disponível para saque."); 
+
     }
 }
